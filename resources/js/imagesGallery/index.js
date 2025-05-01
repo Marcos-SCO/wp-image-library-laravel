0d091fe7dc6ab945bda581a7cf19f83ea3e17187
marcos-sco <marcos_sco@outlook.com>
2025-04-30 23:07:43 -0300

    remove: no used custom debounce function

--- resources/js/imagesGallery/index.js
+++ resources/js/imagesGallery/index.js
@@ -8,14 +8,6 @@ import { handleEditModal } from './_editModal.js';
 import { handleGalleryDeleteItems } from './_deleteModal.js';
 import { dropDownImgUpload } from './_dropDownImgUpload.js';
 
-window.debounce = function debounce(func, delay) {
-  let timeout;
-  return function(...args) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, args), delay);
-  };
-}
-
 window.initEventListeners = function initEventListeners() {
   handlePagination();
   handleEditModal();
