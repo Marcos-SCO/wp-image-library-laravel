b09ad277c3042bba1c106d857e37db385bef912e
marcos-sco <marcos_sco@outlook.com>
2025-05-03 00:15:08 -0300

    feat: page-loading helpers

---
+++ resources/js/imagesGallery/_loading.js
@@ -0,0 +1,37 @@
+function triggerLoadingAnimationFor(element) {
+    if (!element) return;
+    
+    const isHtmlDom = element instanceof HTMLElement;
+
+    const targetItem = isHtmlDom ? element : document.querySelector(element);
+
+    targetItem?.classList.add('page-loading');
+
+    if (targetItem.querySelector('[data-js="loader"]')) return;
+
+    const loader = document.createElement('div');
+    loader.setAttribute('data-js', 'loader');
+    loader.className = 'loader';
+    loader.innerHTML = 'Loading...';
+
+    targetItem?.appendChild(loader);
+}
+
+function removeLoadingAnimationFor(element) {
+    if (!element) return;
+
+    const isHtmlDom = element instanceof HTMLElement;
+
+    const targetItem = isHtmlDom ? element : document.querySelector(element);
+
+    targetItem?.classList.remove('page-loading');
+
+    const loader = targetItem?.querySelector('[data-js="loader"]');
+    if (loader) loader.remove();
+
+}
+
+export {
+    triggerLoadingAnimationFor,
+    removeLoadingAnimationFor,
+}
