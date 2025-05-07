d93df71dbc872c65190863072f5e90976bbab24c
marcos-sco <marcos_sco@outlook.com>
2025-05-07 05:34:48 -0300

    fix: add remaining dummy gallery item slots to avoid layout breaks

--- resources/views/images-gallery/content/gallery-list.blade.php
+++ resources/views/images-gallery/content/gallery-list.blade.php
@@ -45,33 +45,45 @@ $loadingAttribute = $imagesLoopCount <= 3 ? 'eager' : 'lazy' ;
   $imgAlt=objParamExistsOrDefault($image,'alt_text', '' );
   $imgDescription=objParamExistsOrDefault($image, 'description' );
 
-  @endphp
+@endphp
 
-  <div class="gallery-item" data-gallery-item="{{ $imgId }}">
+<div class="gallery-item" data-gallery-item="{{ $imgId }}">
 
-  <figure class="image-wrapper" data-js="image-loading-wrapper">
-    <div class="image-loader"></div>
+<figure class="image-wrapper" data-js="image-loading-wrapper">
+  <div class="image-loader"></div>
 
-    <img
-      src="{{ $imgUrl }}"
-      alt="{{ $imgAlt }}"
-      title="{{ $imgAlt }}"
-      data-img-alt="{{ $imgAlt }}"
-      data-img-description="{{ $imgDescription }}"
-      data-img-path="{{ $imgPath }}"
-      data-img-url="{{ $imgUrl }}"
-      data-img-item="{{ $imgId }}"
-      class="gallery-image"
-      height="266px"
-      width="266px"
-      loading="{{ $loadingAttribute }}">
-  </figure>
+  <img
+    src="{{ $imgUrl }}"
+    alt="{{ $imgAlt }}"
+    title="{{ $imgAlt }}"
+    data-img-alt="{{ $imgAlt }}"
+    data-img-description="{{ $imgDescription }}"
+    data-img-path="{{ $imgPath }}"
+    data-img-url="{{ $imgUrl }}"
+    data-img-item="{{ $imgId }}"
+    class="gallery-image"
+    height="266px"
+    width="266px"
+    loading="{{ $loadingAttribute }}">
+</figure>
 
-  <div class="gallery-actions">
-    <button class="edit-btn" data-js="edit-btn" data-id="{{ $image->id }}">{{ __('Edit') }}</button>
+<div class="gallery-actions">
+  <button class="edit-btn" data-js="edit-btn" data-id="{{ $image->id }}">{{ __('Edit') }}</button>
 
-    <button class="delete-btn" data-js="delete-btn" data-id="{{ $image->id }}">{{ __('Delete') }}</button>
+  <button class="delete-btn" data-js="delete-btn" data-id="{{ $image->id }}">{{ __('Delete') }}</button>
+</div>
+
+</div>
+@endforeach
+
+@php // Add dummy empty slots if less than 12
+$remainingSlots = 12 - $imagesLoopCount;
+@endphp
+
+@for ($i = 0; $i < $remainingSlots; $i++)
+  <div class="gallery-item dummy" aria-hidden="true" style="opacity:0;">
+    <div class="image-wrapper dummy-wrapper">
+    </div>
+    <div class="gallery-actions"></div>
   </div>
-
-  </div>
-  @endforeach
+@endfor
