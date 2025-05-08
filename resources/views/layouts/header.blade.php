465f59074fff6c16c731da37d939ab6390581cb1
marcos-sco <marcos_sco@outlook.com>
2025-05-07 23:07:12 -0300

    refactor: creation of a file for header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,35 @@
+@php use \App\Helpers\Classes\SvgHelper; @endphp
+<header class="page-header py-4 text-white bg-dark position-relative">
+
+    <div class="container position-relative">
+
+        <div class="session-controls-container d-flex justify-content-end align-items-center position-absolute top-0 end-0 mt-3 me-3">
+            @auth
+
+            <div class="auth-username me-3 d-flex align-center">
+                <span>{!! SvgHelper::getSvg('user-icon') !!}</span>
+                <span>{{ auth()->user()->name }}</span>
+            </div>
+
+            <form action="{{ route('logout') }}" method="POST" class="d-inline" hx-boost="false">
+                @csrf
+                <button type="submit" class="btn btn-outline-light btn-sm">
+                    {{ __('Logout') }}
+                </button>
+            </form>
+
+            @else
+            <a href="{{ route('login') }}" class="btn btn-outline-light btn-sm">
+                {{ __('Login') }}
+            </a>
+            @endauth
+        </div>
+
+        <h1 class="text-center mb-4">{{ __('WP Media Library Laravel') }}</h1>
+
+        <nav class="d-flex justify-content-center gap-3 pt-2">
+            <a href="{{ route('gallery.index') }}" class="btn btn-light py-2 px-4">{{ __('Main library page') }}</a>
+            <a href="{{ route('gallery.index') }}" class="btn btn-light py-2 px-4">{{ __('Open Modals demonstration') }}</a>
+        </nav>
+    </div>
+</header>
