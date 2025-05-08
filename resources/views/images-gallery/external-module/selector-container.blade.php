3bb7bc18a5085139e9f658f67e20e0fd306f9f7a
marcos-sco <marcos_sco@outlook.com>
2025-05-08 01:21:19 -0300

    fix: get the proper svg icon

--- resources/views/images-gallery/external-module/selector-container.blade.php
+++ resources/views/images-gallery/external-module/selector-container.blade.php
@@ -1,9 +1,10 @@
+@php use \App\Helpers\Classes\SvgHelper; @endphp
 <!-- Modal for Gallery Selection -->
 <div class="gallery-modal gallery-external-modal lightbox-modal" data-js="gallery-selector-modal">
   <input type="hidden" name="base-url" data-base-url="{{ url('') }}">
 
   <div class="modal-content">
-    <button class="close-button" data-js="close-gallery-modal" close-modal>{!! App\Helpers\Classes\SvgHelper::getSvg('close-icon', 'assets/svg'); !!} </button>
+    <button class="close-button" data-js="close-gallery-modal" close-modal>{!! SvgHelper::getSvg('close-icon'); !!} </button>
     <!-- Modal content will be dynamically loaded here -->
     <div data-js="modal-gallery-content"></div>
   </div>
