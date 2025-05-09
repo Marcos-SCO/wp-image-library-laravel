55e13510e3fac0ba98764414fdff215f1ba55215
marcos-sco <marcos_sco@outlook.com>
2025-05-08 22:43:54 -0300

    style: change project name

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# wp-media-library-laravel
+# wp-image-library-laravel
 Recreation of WP Media library in Laravel
+
+php artisan storage:link
 
 php artisan migrate:fresh --seed
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{{ env("APP_NAME", "Wp Media Library Laravel") }}</title>
+  <title>{{ env("APP_NAME", "Wp Image Library Laravel") }}</title>
 
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
